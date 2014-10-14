614dc5b8a29ee4aae1eebb2bdd836a18aa05577c
Laupetin <laupetin@web.de>
2014-10-14 13:36:34 +0200

    :octocat: Basisgerüst

---
+++ src/main/java/de/Laupetin/ElementalSouls/Proxies/ESClientProxy.java
@@ -0,0 +1,11 @@
+package de.Laupetin.ElementalSouls.Proxies;
+
+public class ESClientProxy extends ESCommonProxy {
+
+	@Override
+	public void registerRenderers() 
+	{
+		
+	}
+	
+}
---
+++ src/main/java/de/Laupetin/ElementalSouls/Proxies/ESCommonProxy.java
@@ -0,0 +1,11 @@
+package de.Laupetin.ElementalSouls.Proxies;
+
+public class ESCommonProxy {
+
+	public void registerRenderers()
+	{
+		
+		
+	}
+	
+}
