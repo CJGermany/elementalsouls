6c8700f70e7ee6c2cd9134a163fb0cdda7a9b019
Laupetin <laupetin@web.de>
2014-10-19 15:12:00 +0200

    Test waterwalk

--- src/main/java/de/Laupetin/ElementalSouls/Event/ESEventHandler.java
+++ src/main/java/de/Laupetin/ElementalSouls/Event/ESEventHandler.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.ServerTickEvent;
 import cpw.mods.fml.relauncher.Side;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
 
 public class ESEventHandler {
 
@@ -29,6 +33,40 @@ public class ESEventHandler {
 		for(int i = 0; i < gtr.size(); i++){
 			gtr.get(i).onGameTick(Side.SERVER);
 		}
+	}
+	
+	@SubscribeEvent
+	public void onPlayerTick(PlayerTickEvent event){
+		System.out.println("PlayerTick");
+		boolean waterWalk = false;	// Ist WaterWalk im Moment aktiviert?
+		double waterMana = 100;	// wie viel Wasserladung hat der Spieler aktuell?
+		double ticksPerSecond = 20;	// Wie kann man die sonst abfragen? Bei overload sind die ja nicht immer ganz stabil...
+		double waterWalkManaConsumption = 10/ticksPerSecond;	// Manaverbrauch pro Sekunde (Ist ja variabel je nach Rüstung und Skills...)
+		
+		// waterWalk = p.getAbilities("waterWalk").isEnabled();	//Wir brauchen ein system, um einfach solche abfragen machen zu können.
+		// waterMana = p.getMana("water");	// Muss alles nicht unbedingt genau so aussehen, aber so ist es denk ich mal plausibel und einfach aufgebaut.
+		// waterWalkManaConsumption = p.getAbilities("waterWalk").getConsumptionRate();
+		
+		if (waterWalk && (waterMana > waterWalkManaConsumption)) {
+			// Hier kann man auf dem Wasser laufen.
+			World world = event.player.worldObj;
+			
+			int x = MathHelper.floor_double(event.player.posX);
+			int y = MathHelper.floor_double(event.player.boundingBox.minY - 1);
+			int z = MathHelper.floor_double(event.player.posZ);
+			
+			Material maybeWater = world.getBlock(x, y, z).getMaterial();
+			
+			boolean water = (maybeWater == Material.water);
+			
+			if (water && (event.player.motionY < 0.0D)) {
+				event.player.posY += -event.player.motionY;
+				event.player.motionY = 0.0D;
+				event.player.fallDistance = 0.0F;
+				// player.setMana("water", waterMana-(waterWalkManaConsumption/ticksPerSecond))
+			}
+		}
+		
 	}
 	
 	public void registerGameTickReceiver(ESGameTickReceiver g){
